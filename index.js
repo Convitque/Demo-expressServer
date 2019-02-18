9a10f663d8cd680216421ca08d22143432ec03a4
Vit <huuthang.uit2016@gmail.com>
2019-02-19 04:46:07 +0700

    Simple Validation

--- index.js
+++ index.js
@@ -5,9 +5,13 @@ var userRoute = require('./routes/user.route')
 
 const port = 3000
 
-const app = express()
+const app = express();
+
+
+
 app.set('view engine', 'pug')
 app.set('views', './views')
+
 
 
 app.use(bodyParser.json()); // for parsing application/json
@@ -19,52 +23,11 @@ app.set('view engine', 'pug')
 app.set('views', './views')
 
 
+
 app.get('/', function (req, res) {
     res.render('index');
 });
 
 app.use('/users',userRoute);
-// app.get('/users', function (req, res) {
-//     res.render('users/user', {
-//         users: db.get('users').value()
-//     })
-// });
-
-
-
-// app.get('/users/search', function (req, res) {
-//     var q = req.query.q;
-
-//     var mathedUsers = db.get('users').value().filter(function(user) {
-//         return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
-//     });
-
-//     console.mathedUser;
-//     res.render('users/user', {
-//         users: mathedUsers
-//     })
-// })
-
-// app.get('/users/create', function (req, res) {
-//     res.render('users/create');
-// })
-
-
-// app.get('/users/:id', function (req, res) {
-//     var id = req.params.id;
-
-//     var user = db.get('users').find({
-//         id: id
-//     }).value();
-//     res.render('users/view', {
-//         user: user
-//     })
-// })
-
-// app.post('/users/create', function (req, res) {
-//     req.body.id = shortid.generate();
-//     db.get('users').push(req.body).write();
-//     res.redirect('/users');
-// })
 
 app.listen(port, () => console.log('Server is listening on port: ' + port));
